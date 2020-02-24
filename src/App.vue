e23a5b05416040a3114601e31c38213eaa9cbe2d
HoSeokMoon <adele007fall@naver.com>
2020-02-24 23:46:26 +0900

    vuetify 조작중

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
+    <Header></Header>
     <User></User>
   </div>
 </template> 
 <script>
 import User from './components/User';
+import Header from './Header'
+
 import './assets/css/reset.css'
 
 export default {
@@ -12,6 +15,7 @@ export default {
 
   components: {
     User,
+    Header
   },
 
   data: () => ({
--- src/Header.vue
+++ src/Header.vue
@@ -1,11 +1,57 @@
 <template>
+<!-- 헤더 로고 wrap -->
+    <v-card class="overflow-hidden"
     
-</template>
+      extension-height="260px"
+      height="260px" >
+    <v-app-bar 
+      absolute
+      color="indigo darken-2" 
+      shrink-on-scroll
+      prominent
+      scroll-target="#scrolling-techniques"
+    >
+      <v-img class="logo"></v-img>
+      <v-spacer></v-spacer>
 
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container class="contain" style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+<style scoped> 
+  .theme--light.v-app-bar.v-toolbar.v-sheet {
+    background-color: #0058a3; 
+} 
+.contains {
+  padding: auto;
+}
+  .logo {
+    background: url(./assets/icon/ikea_logo.svg) no-repeat center/contain;
+    width: 110px;
+    height: 56px;
+  }
+</style>
 <script>
 export default {
     data(){
 
     }
 }
-</script>
+</script> 
